8c6d3271bd21dbf6bd2d953616a206f91fa23a86
Offichigo <oph.perianin@gmail.com>
2026-02-11 11:41:19 +0100

    ajout dossier Exercices

---
+++ Exercices-js/Exercice-1-variables-js/script.js
@@ -0,0 +1,7 @@
+console.log("Bonsoir!!!!");
+
+const firstName = "Off";
+const age = 35;
+
+console.log("Je m'appelle " + firstName + " et j'ai " + age + " ans.");
+//Je m'appelle Off et j'ai 35 ans.
