93753c55f64450dbf46754b4c916f37ba37da540
Offichigo <oph.perianin@gmail.com>
2026-02-11 14:46:36 +0100

    exercice js conditions

---
+++ Exercices-js/Exercice-2-fonctions-js/script.js
@@ -0,0 +1,12 @@
+function addition3Numbers() {
+  const resultat = 1 + 2 + 3;
+  console.log("Le résultat de la somme est " + resultat);
+}
+addition3Numbers();
+
+function addition3NumbersWithArg(a, b, c) {
+  co;
+}
+
+let resultat = addition3NumbersWithArg(a, b, c);
+console.log(resultat);
