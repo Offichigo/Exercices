a04dbc9bf7f6dd8453d36d3223c1b4f0d096976b
Offichigo <oph.perianin@gmail.com>
2026-02-12 16:22:10 +0100

    exercice 2 condition fait

---
+++ Exercices-js/Exercices-3-Conditions-js/Exercice-2/script.js
@@ -0,0 +1,8 @@
+function nameage(name, age) {
+  if (age < 17) {
+    console.log(name + " n'a pas l'âge pour conduire ");
+  } else if ((age) => 17) console.log(name + " peut conduire.");
+}
+nameage("Mathieu", 22);
+nameage("Léa", 15);
+nameage("Jean", 17);
