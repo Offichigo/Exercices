4d29995f3e0e0f97d86df21e4a6807cbfd1a6b19
Offichigo <oph.perianin@gmail.com>
2026-02-17 20:01:49 +0100

    exercice 3 les boucles fini

--- Exercices-js/Exercices-5-Les-boucles/Exercice-3/script.js
+++ Exercices-js/Exercices-5-Les-boucles/Exercice-3/script.js
@@ -1,17 +1,25 @@
 // Nous souhaitons créer un décompte.
-
 //     Demande à l'utilisateur (à l'aide de prompt) un nombre entre 0 et 10.
 //     Affiche ensuite tous les nombres sous le nombre indiqué par l'utilisateur (s'il rentre 4, on affichera 4, 3, 2, 1, 0).
-
 // Attention, si l'utilisateur rentre un chiffre erroné il faudra afficher message d'erreur.
 
-function numbers(countdown) {
-  let give = prompt("Donne un nombre entre 0 et 10");
-  for (let position = 10; position > -1; position--) {
+let give = prompt("Donne un nombre entre 0 et 10");
+give = parseInt(give);
+
+if (give >= 0 && give <= 10) {
+  showCountdown(give);
+} else {
+  console.log("Relis la consigne!");
+}
+
+//faire une condition pour tester que give est bien un nombre sinon n'appelle pas la fonction
+
+function showCountdown(countdown) {
+  for (let position = countdown; position > -1; position--) {
     console.log("Count: ", position);
-    if (give > 4) {
-      console.log();
-    }
   }
 }
-numbers(prompt);
+
+//Fonction montre dans la console d'un décompte à partir du numéro donné par l'utilisateur en paramètre
+//stocké le résultat du prompt dans give
+//apellé la fonction en donnant le paramètre
