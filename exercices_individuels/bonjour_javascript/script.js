bbd01d59e2be2292b75ed32399c518fc4add7cad
Offichigo <oph.perianin@gmail.com>
2026-03-01 19:22:55 +0100

    fini

--- exercices_individuels/bonjour_javascript/script.js
+++ exercices_individuels/bonjour_javascript/script.js
@@ -1,15 +1,16 @@
-// function sayHello(firstName, hour) {
-//   let message = "Bonjour ";
-//   let message2 = "Bonsoir ";
-//   if (hour >= 18) {
-//     console.log(`${message2}${firstName} !`);
-//   } else {
-//     console.log(`${message}${firstName} !`);
-//   }
-// }
-// sayHello(`Beyonce`, 11);
-// sayHello(`Beyonce`, 18);
-// sayHello(`Beyonce`, 17);
+function sayHello(firstName, hour) {
+  let message = "Bonjour ";
+  let message2 = "Bonsoir ";
+  if (hour >= 18) {
+    console.log(`${message2}${firstName} !`);
+  } else {
+    console.log(`${message}${firstName} !`);
+  }
+}
+sayHello(`Beyonce`, 11);
+sayHello(`Beyonce`, 18);
+sayHello(`Beyonce`, 17);
+
 const givename = prompt("donne ton prénom");
 
 function sayHello2(nameUser) {
