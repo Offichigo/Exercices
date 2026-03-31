907d04d617074a27b26591030a63b3a6e18083af
Offichigo <oph.perianin@gmail.com>
2026-03-31 18:09:14 +0200

    open meteo js quasi fini

--- api_exo/src/calling/open-meteo.js
+++ api_exo/src/calling/open-meteo.js
@@ -1,39 +1,17 @@
 /**
- * const cities = {
-  Nantes: {
-    lat: 47.2184,
-    lon: -1.5536
-  },
-  Paris: {
-    lat: 48.8566,
-    lon: 2.3522
-  },
-  Rennes: {
-    lat: 48.1173,
-    lon: -1.6778
-  },
-  Caen: {
-    lat: 49.1829,
-    lon: -0.3707
-  },
-  Marseille: {
-    lat: 43.2965,
-    lon: 5.3698
-  }
-};
  * appel l'API Open-Meteo
  * Doc : https://open-meteo.com/en/docs
  * @param {number} lat latitude
  * @param {number} lon longitude
  * @returns retourne les données fournies par l'API. (pour données actuelles, appeler current_weather)
  */
+
 export async function callOpenMeteoAPI(lat, lon) {
-  const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true`;
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true&daily=temperature_2m_max,temperature_2m_min&timezone=Europe/Paris`;
   try {
-    const reponse = await fetch(url);
-    const resultat = await reponse.json();
-    console.log(resultat);
-    return resultat;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
   } catch (err) {
     console.error(err);
   }
--- api_exo/src/main.js
+++ api_exo/src/main.js
@@ -4,12 +4,53 @@ import "./style.css";
 /**
  * Lance la requête vers l'API et remplace les données une fois obtenue.
  */
-let renderOpenMeteo = async () => {
-  // TODO
+const cities = {
+  Nantes: {
+    lat: 47.2184,
+    lon: -1.5536,
+  },
+  Paris: {
+    lat: 48.8566,
+    lon: 2.3522,
+  },
+  Rennes: {
+    lat: 48.1173,
+    lon: -1.6778,
+  },
+  Caen: {
+    lat: 49.1829,
+    lon: -0.3707,
+  },
+  Marseille: {
+    lat: 43.2965,
+    lon: 5.3698,
+  },
 };
 
+//récupérer le li
+//changer le texte du li
+// Afficher la météo
+
+let renderOpenMeteo = async () => {
+  Object.entries(cities).forEach(([name, details]) => {
+    const bouton = document.getElementById(name.toLowerCase());
+    bouton.addEventListener("click", async () => {
+      const data = await callOpenMeteoAPI(details.lat, details.lon);
+      const currentWeather = data.current_weather.temperature;
+      const lis = document.querySelectorAll("li");
+      const dayWeather = document.querySelector(".list");
+      lis.forEach((li, index) => {
+        li.innerText = `${data.daily.time[index]} : Température min : ${data.daily.temperature_2m_min[index]}°C, Température max : ${data.daily.temperature_2m_max[index]}°C`;
+      });
+      dayWeather.innerText = `Actuellement à ${name} il fait : ${currentWeather}°C`;
+    });
+  });
+};
+//récupérer les li de chaque jour météo
+//changer le texte de chaque li
+//récupérer température
+//afficher la météo de chaque jour du li
 // =====================================================================
 // - - - - - - - - - - - E X E C U T I O N - - - - - - - - - - - - - - -
 // =====================================================================
 renderOpenMeteo();
-callOpenMeteoAPI();
