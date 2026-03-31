011e3409fc67f62e4e0ea2bd4d437fdfbfb57b4d
Offichigo <oph.perianin@gmail.com>
2026-03-31 22:07:21 +0200

    hi

--- api_exo/vite.config.js
+++ api_exo/vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/Exercices/",
   server: {
     proxy: {
       "/lba": {
         target: "https://api.apprentissage.beta.gouv.fr",
         changeOrigin: true,
         secure: true,
-        rewrite: (path) => path.replace(/^\/lba/, "")
-      }
-    }
-  }
+        rewrite: (path) => path.replace(/^\/lba/, ""),
+      },
+    },
+  },
 });
